e75791420bd1c4e3597481b5b736397962826824
danteidbratt <32329674+danteidbratt@users.noreply.github.com>
2018-07-09 23:28:26 +0200

    Fix

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -14,40 +14,53 @@ import java.util.List;
 
 public class Main {
 
-    List<Algorithm> algorithms;
-    private final Retreivable data;
-    private final Window window;
+    private Retreivable data;
+    private Window window;
+    private List<Speed> speeds;
+    private List<Resolution> resolutions;
+    private Theme theme;
 
-    public Main() {
-        List<Speed> speeds = Arrays.asList(
+    private void start() {
+        loadSettings();
+        loadData();
+        loadView();
+        loadAlgorithms();
+    }
+
+    private void loadSettings() {
+        speeds = Arrays.asList(
                 new Speed("Low", 300),
                 new Speed("Mid", 100),
                 new Speed("High", 10),
                 new Speed("Ultra", 1)
         );
-        List<Resolution> resolutions = Arrays.asList(
+        resolutions = Arrays.asList(
                 new Resolution("Low", 16),
                 new Resolution("Mid", 64),
                 new Resolution("High", 128),
                 new Resolution("Ultra", 256)
         );
-        Theme theme = new Theme(Color.YELLOW, Color.DARK_GRAY, new Color(50, 50, 50), Color.GREEN);
+        theme = new Theme(Color.YELLOW, Color.DARK_GRAY, new Color(50, 50, 50), Color.GREEN, Color.WHITE);
+    }
+
+    private void loadData() {
         data = new Data();
         data.loadValues(resolutions.get(0).getValue());
-        window = new Gui(speeds, resolutions, theme);
-        algorithms = new Setup(window, data).getSortingAlgorithms();
+    }
 
-        window.setAlgorithms(algorithms);
+    private void loadView() {
+        window = new Gui(speeds, resolutions, theme);
         window.setShuffleListener(e -> window.updateBars(data.shuffleValues()));
         window.updateBars(data.getValues());
     }
 
-    private void start() {
-
+    private void loadAlgorithms() {
+        window.setAlgorithms(new Setup(window, data).loadAlgorithms());
     }
 
     public static void main(String[] args) {
         Main main = new Main();
         main.start();
     }
+
 }
